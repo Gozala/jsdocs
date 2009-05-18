0eca8f9aacff700e037c368de445a735386c5908
Irakli Gozalishvili <rfobic@gmail.com>
2009-05-18 03:46:24 +0200

    Starting work on platform specific parsers. Rhino will use it's own parser to build abstract syntax trees and retrieve comments

---
+++ platforms/rhino/jsdocs/ast.js
@@ -0,0 +1,170 @@
+/**
+ * @Module
+ * Abstaract Syntax Tree
+ */
+
+/**
+ * shortcuts
+ */
+var JAstNode = org.mozilla.javascript.ast.AstNode;
+
+/**
+ * Node for the root of a parse tree.  It contains the statements and functions
+ * in the script, and a list of {@link Comment} nodes associated with the script
+ * as a whole. Node type is {@link Token#SCRIPT}.
+ *
+ * Note that the tree itself does not store errors. Parse errors
+ * and warnings, can be found in {@link Parser#errors} {@link Parser#warnings}.
+ * @class
+ * @author Irakli Gozalishvili
+ * @param {org.mozilla.javascript.ast.AstRoot} jAst
+ */
+function AstRoot(jAst) {
+    if (!jAst) return null;
+    this._ast = jAst;
+}
+AstRoot.prototype = {
+    /**
+     * @type org.mozilla.javascript.ast.AstRoot
+     */
+    _ast: null,
+    /**
+     * @param {Array}       instance to collect ast nodes in.
+     * Creates simple visitor for traversing the AST. Used to collect nodes
+     * from the {@code org.mozilla.javascript.ast.AstRoot} instance.
+     * Implements {@code org.mozilla.javascript.ast.NodeVisitor}
+     */
+    _getVisitor: function(nodes) {
+        return function(node) {
+            nodes.push(new AstNode(node));
+            return true;
+        };
+    },
+    /**
+     * URI, path or descriptive text indicating the origin
+     * of this script's source code.
+     * @type {String}
+     */
+    get uri() {return new String(this._ast.getSourceName);},
+    /**
+     * Visits the all nodes expect comment ones in the order they appear in the
+     * source code and replaces self (getter) with an array (property).
+     * @param {AstNode[]}
+     */
+    get nodes() {
+        var self = arguments.callee;
+        if (!self.nodes) {
+            self.nodes = [];
+            this._ast.visit(this._getVisitor(self.nodes));
+        }
+        return self.nodes;
+    },
+    /**
+     * Visits the comment nodes in the order they appear in the source code.
+     * And replaces self (getter) with an array (property).
+     * @param {AstNode[]}
+     */
+    get commentNodes() {
+        var self = arguments.callee;
+        if (!self.nodes) {
+            self.nodes = [];
+            this._ast.visitComments(this._getVisitor(self.nodes));
+        }
+        return self.nodes;
+    },
+    /**
+     * @returns {String}
+     */
+    toSource: function() { return new String(this._ast.toSource()); }
+};
+exports.AstRoot = AstRoot;
+
+/**
+ * Base class for AST node types. The goal of the AST is to represent the
+ * physical source code, to make it useful for code-processing tools such
+ * as IDEs or pretty-printers.  The parser must not rewrite the parse tree
+ * when producing this representation.
+ * @class
+ * @param {org.mozilla.javascript.ast.AstNode}
+ */
+function AstNode(jAstNode) {
+    if (!jAstNode) return null;
+    this._astNode = jAstNode;
+}
+AstNode.prototype = {
+    /**
+     * @type org.mozilla.javascript.ast.AstNode
+     */
+    _astNode: null,
+    /**
+     * Returns a debugging representation of the parse tree
+     * starting at this node.
+     * @returns {String}        a very verbose indented printout of the tree.
+     * The format of each line is:  abs-pos  name position length [identifier]
+     */
+    debugPrint: function() { return new String(this._astNode.debugPrint()); },
+    /**
+     * Line number recorded for this node. If no line number was recorded,
+     * searches the parent chain. the nearest line number, or -1 if none was
+     * found
+     * @type Integer
+     */
+    get line() { return parseInt(new String(this._astNode.getLineno())) },
+    /**
+     * Depth of this node. The root is depth 0, its children are depth 1, and
+     * so on.
+     * @type Integer
+     */
+    get depth() { return parseInt(new String(this._astNode.depth())) },
+    /**
+     * Short, descriptive name for the node, such as "ArrayComprehension".
+     * @type String
+     */
+    get shortName() { return new String(this._astNode.shortName()); },
+    /**
+     * Prints the source indented to depth 0.
+     */
+    toSource: function() { return new String(this._astNode.toSource()); },
+    /**
+     * Returns the root of the tree containing this node.
+     * @return the {@link AstRoot} at the root of this node's parent
+     * chain, or {@code null} if the topmost parent is not an {@code AstRoot}.
+     * @type AstRoot
+     */
+    get astRoot() { return new AstRoot(this._astNode.getAstRoot()) },
+    /**
+     * Parent node, or {@code null} if it has none.
+     */
+    get parent() { return new AstNode(this._astNode.getParent()); },
+    /**
+     * Node length
+     * @type Integer
+     */
+    get length() { return parseInt(new String(this._astNode.getLength())) },
+    /**
+     * Absolute document position of the node. Computes it by adding the node's
+     * relative position to the relative positions of all its parents.
+     * @type Integer
+     */
+    get absolutePosition() {
+        return parseInt(new String(this._astNode.getAbsolutePosition()))
+    },
+    /**
+     * Relative position in parent
+     * @type Integer
+     */
+    get position() { return parseInt(new String(this._astNode.getPosition())) },
+    toString: function() {
+        
+    }
+};
+/**
+ * Returns the string name for this operator.
+ * @param {Integer}         op the token type, e.g. {@link Token#ADD} or
+ * {@link Token#TYPEOF}
+ * @returns {String}        the source operator string, such as "+" or "typeof"
+ */
+AstNode.operatorToString = function operatorToString(operator) {
+    return new String(JAstNode.operatorToString(operator))
+};
+exports.AstNode = AstNode;
--- platforms/rhino/jsdocs/parser.js
+++ platforms/rhino/jsdocs/parser.js
@@ -1,4 +1,22 @@
-var File = require('file').Path;
+/**
+ * @module
+ * @requires ast
+ * @author Irakli Gozalishvili <rfobic@gmail.com>
+ */
+
+/**
+ * @imports
+ */
+var File = require('file').Path
+    AstRoot = require('jsdocs/ast').AstRoot;
+
+/**
+ * shortcuts
+ */
+var JParser = org.mozilla.javascript.Parser;
+var JCompilerEnvirons = org.mozilla.javascript.CompilerEnvirons;
+var JErrorReporter = org.mozilla.javascript.ErrorReporter;
+var JEvaluatorException = org.mozilla.javascript.EvaluatorException;
 
 /**
  * This class implements the JavaScript parser.
@@ -7,17 +25,89 @@ var File = require('file').Path;
  * 
  * @class This class implements jsavascript parser.
  * @see TokenStream
- * @author Irakli Gozalishvili
+ * @author Irakli Gozalishvili <rfobic@gmail.com>
  */
 function Parser() {
-    RhinoParser = org.mozilla.javascript.Parser;
-    this._parser = new RhinoParser();
+    var errors = this.errors = [];
+    var warnings = this.warnings = [];
+    /**
+     * Implementing java interface {@code org.mozilla.javascript.ErrorReporter}
+     */
+    var errorReporter = new JErrorReporter({
+        /**
+         * Reports a warning.
+         * @param {java.lang.String} message        warning description
+         * @param {java.lang.String} sourceName     JavaScript source description
+         * where the warning occured; typically a filename or URL
+         * @param {java.Int} line                   line number associated with
+         * the warning
+         * @param {java.lang.String} lineSource     the text of the line
+         * @param {java.Int} lineOffset             the offset into lineSource
+         * where problem was detected
+         */
+        warning: function(message, sourceName, line, lineSource, lineOffset) {
+            warnings.push({
+                message: message,
+                sourceName: sourceName,
+                line: line,
+                lineSource: lineSource,
+                lineOffset: lineOffset
+            });
+        },
+        /**
+         * Reports an error.
+         * @param {java.lang.String} message        warning description
+         * @param {java.lang.String} source         JavaScript source description
+         * where the warning occured; typically a filename or URL
+         * @param {java.Int} line                   line number associated with
+         * the warning
+         * @param {java.lang.String} lineSource     the text of the line
+         * @param {java.Int} cffset             the offset into lineSource
+         * where problem was detected
+         */
+        error: function(message, sourceName, line, lineSource, lineOffset) {
+            errors.push({
+                message: message,
+                sourceName: sourceName,
+                line: line,
+                lineSource: lineSource,
+                lineOffset: lineOffset
+            });
+        },
+        /**
+         * Creates an EvaluatorException that will be thrown by Parser.
+         * runtimeErrors, unlike errors, will always terminate the current
+         * script.
+         * @param {java.lang.String} message        warning description
+         * @param {java.lang.String} sourceName     JavaScript source description
+         * where the warning occured; typically a filename or URL
+         * @param {java.Int} line                   line number associated with
+         * the warning
+         * @param {java.lang.String} lineSource     the text of the line
+         * @param {java.Int} lineOffset             the offset into lineSource
+         * where problem was detected
+         * @returns org.mozilla.javascript.EvaluatorException
+         */
+        runtimeError: function(message, sourceName, line, lineSource, lineOffset) {
+            return new JEvaluatorException(message + ' : ' +
+                lineSource +  ': line ' + line + '  at ' + lineOffset);
+        }
+    });
+    this._parser = new JParser(new JCompilerEnvirons(), errorReporter);
 };
 Parser.prototype = {
     /**
      * @type org.mozilla.javascript.Parser
      */
     _parser: null,
+    /**
+     *
+     */
+    errors: null,
+    /**
+     *
+     */
+    warnings: null,
     /*
      * Build a parse tree from the given source.
      *
@@ -32,7 +122,11 @@ Parser.prototype = {
         if (source.read) source = source.read().toString();
         uri = uri || 'anonymus';
         lineno = lineno || 0;
-        this._parser.parse(source, uri, lineno)
+        try {
+            return new AstRoot(this._parser.parse(source, uri, lineno));
+        } catch(e) {
+            throw new Error(new String(e.message));
+        }
     }
 };
 exports.Parser = Parser;
