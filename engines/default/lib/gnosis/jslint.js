81e93f9a47650ce5b52c5a3984ce72cdf47ee4c3
Irakli Gozalishvili <rfobic@gmail.com>
2010-01-28 23:07:47 +0100

    Removeing all the useless stuff and leaving only files ported from jsdoc-toolkit

--- engines/default/lib/gnosis/jslint.js
+++
@@ -1,7 +0,0 @@
-eval(require("file").path(module.path).join("../fulljslint.js").read().toString());
-exports.JSLINT = function(source) {
-    if (source.read) source = source.read();
-    JSLINT(source.toString());
-    return JSLINT;
-}
-
